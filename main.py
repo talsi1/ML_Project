635f8470c8e49e2a49d66b6590c3e7c74e7383be
Tal Simon <talsimon@Tals-MacBook-Pro-6.local>
2016-11-17 23:11:15 +0200

    updated the script

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ from sklearn import svm
 #######################Feature Extraction Process###############
 
 
-def featuresExtraction (movement_class, path):
-    feature_num = 34
+def featuresExtraction (movement_class, path, feature_num):
     allFiles = glob.glob(path + "/*")
     list_ = np.zeros((len(allFiles), feature_num))
     i = 0
@@ -38,22 +37,22 @@ def featuresExtraction (movement_class, path):
         list_[i, 0:6] = temp.mean().reshape(1,6)
         list_[i, 6:12] = temp.std().reshape(1,6)
         list_[i, 12:18] = temp.median().reshape(1,6)
-        list_[i, 18:19] = np.fft.fftn(temp).max()
-        list_[i, 19:20] = np.fft.fftn(temp).min()
-        list_[i, 20:26] = sp.stats.skew(np.fft.fftn(temp)).reshape(1,6)
-        list_[i, 26:32] = sp.stats.kurtosis(np.fft.fftn(temp)).reshape(1,6)
-        list_[i, 33] = sp.stats.iqr(temp)
+        #list_[i, 18:19] = np.fft.fftn(temp).max()
+        #list_[i, 19:20] = np.fft.fftn(temp).min()
+        #list_[i, 20:26] = sp.stats.skew(np.fft.fftn(temp)).reshape(1,6)
+        #list_[i, 26:32] = sp.stats.kurtosis(np.fft.fftn(temp)).reshape(1,6)
+        #list_[i, 33] = sp.stats.iqr(temp)
         list_[i, feature_num - 1] = movement_class
         i = i + 1    
 
     return list_
 
 ###############################################################
+feature_num = 19
 
-
-X_squat = pd.DataFrame(featuresExtraction(0, r'/Users/talsimon/Desktop/Machine Learning/Squat'))  
-X_sp = pd.DataFrame(featuresExtraction(1, r'/Users/talsimon/Desktop/Machine Learning/SP'))
-X_dl = pd.DataFrame(featuresExtraction(2, r'/Users/talsimon/Desktop/Machine Learning/Deadlift'))
+X_squat = pd.DataFrame(featuresExtraction(0, r'/Users/talsimon/Desktop/Machine Learning/Squat', feature_num))  
+X_sp = pd.DataFrame(featuresExtraction(1, r'/Users/talsimon/Desktop/Machine Learning/SP', feature_num))
+X_dl = pd.DataFrame(featuresExtraction(2, r'/Users/talsimon/Desktop/Machine Learning/Deadlift', feature_num))
 
 X = pd.DataFrame()       
 X = X.append(X_squat)
@@ -63,24 +62,21 @@ X = X.append(X_dl)
 X = np.matrix(X.values)
 np.random.shuffle(X)
 
-raw_y = pd.DataFrame(X)
-raw_y = raw_y.drop(raw_y.columns[:33], axis=1)
-
-#y = np.matrix(y.values)
-
-raw_data_x = pd.DataFrame(X)
-raw_data_x = raw_data_x.drop(raw_data_x.columns[33], axis=1)
-#raw_data_y = pd.DataFrame(y)
-
 # append a ones column to the front of the data set
+raw_data_x = pd.DataFrame(X)
 raw_data_x.insert(0, 'Ones', 1)
 
 # set X (training data) and y (target variable)
 cols = raw_data_x.shape[1]  
-X = raw_data_x.iloc[:200,0:cols-1]  
-y = raw_y.iloc[:200,0]
-Xval = raw_data_x.iloc[200:290,0:cols-1]
-yval = raw_y.iloc[200:290,0]
+
+X = np.matrix(raw_data_x.iloc[:200,:cols-1])  
+Xval = np.matrix(raw_data_x.iloc[200:290,:cols-1])
+
+y = raw_data_x.iloc[:200,cols-1:cols]
+yval = raw_data_x.iloc[200:290,cols-1:cols]
+
+y = np.array(y.values).ravel()
+yval = np.array(yval.values).ravel()
 
 
 C_values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100]  
